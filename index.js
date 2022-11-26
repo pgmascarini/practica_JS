5563d1c769621680697639783455e65dd3d5175d
pgmascarini <74653483+pgmascarini@users.noreply.github.com>
2022-11-26 10:13:16 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const students = [{
+  age: 32,
+  examScores: [],
+  gender: 'male',
+  name: 'edu'
+},
+{
+  age: 29,
+  examScores: [],
+  gender: 'female',
+  name: 'silvia'
+}]
+
+const availableMaleNames = ['pepe', 'juan', 'victor', 'Leo', 'francisco', 'carlos'];
+const availableFemaleNames = ['cecilia', 'ana', 'luisa', 'silvia', 'isabel', 'virginia'];
+const availableGenders = ['male', 'female'];
